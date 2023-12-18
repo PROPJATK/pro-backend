9b9aef96657c9274fdc610079a074e6e8ef4065e
MarMarcz <s24566@pjwstk.edu.pl>
2023-12-18 15:52:40 +0100

    Adding new endpoints

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const databaseConnection = require('./DataBaseConnection');
 const countriesGET = require('./Endpoints/CountriesGET');
+const restaurantsGET = require('./Endpoints/RestaurantsGET');
+const hotelsGET = require('./Endpoints/HotelsGET');
 
 const app = express();
 const port = 3000;
@@ -8,6 +10,8 @@ const port = 3000;
 databaseConnection();
 
 app.use(countriesGET);
+app.use(restaurantsGET);
+app.use(hotelsGET);
 
 app.listen(port, () => {
     console.log(`Application works on port: ${port}`);
