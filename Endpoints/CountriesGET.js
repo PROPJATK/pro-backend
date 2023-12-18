62289b246a93b01a4d60b39686288d487bcadc51
s24531 <91957063+s24531@users.noreply.github.com>
2023-12-18 15:54:09 +0100

    adding new endpoint

--- Endpoints/CountriesGET.js
+++ Endpoints/CountriesGET.js
@@ -4,8 +4,9 @@ const TravelAppCountriesModel = require('../Schemas/CountriesSchema');
 
 router.get('/countries', async (req, res) => {
     try {
-        const documents = await TravelAppCountriesModel.find({},'-_id');
-    } catch (error) {
+        const documents = await TravelAppCountriesModel.find({},'name image -_id');
+        res.send(documents);
+            } catch (error) {
         console.error('Error retrieving data from collection:', error);
         res.status(500).send('Internal Server Error');
     }
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const databaseConnection = require('./DataBaseConnection');
 const countriesGET = require('./Endpoints/CountriesGET');
+const attractionsGET = require('./Endpoints/AttractionGET');
 
 const app = express();
 const port = 3000;
@@ -8,6 +9,7 @@ const port = 3000;
 databaseConnection();
 
 app.use(countriesGET);
+app.use(attractionsGET);
 
 app.listen(port, () => {
     console.log(`Application works on port: ${port}`);
