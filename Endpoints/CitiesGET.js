0f9cf7c5ec7dbded855685768d666d3d1b2b0d91
wojciechosmulski <34412964+wojciechosmulski@users.noreply.github.com>
2023-12-18 18:54:04 +0100

    refactor(endpoints): add swagger and re-route

--- Endpoints/CitiesGET.js
+++ Endpoints/CitiesGET.js
@@ -1,20 +1,46 @@
-const express = require('express');
-const router = express.Router();
-const TravelAppCountriesModel = require('../Schemas/CountriesSchema');
-
-router.get('/:country/Cities', async (req, res) => {
-    const countryName = req.params.country;
+const express = require('express')
+const router = express.Router()
+const TravelAppCountriesModel = require('../Schemas/CountriesSchema')
+/**
+ * @swagger
+ * /api/countries/{countryName}/cities:
+ *  get:
+ *    description: Use to request all cities of a country
+ *    parameters:
+ *      - name: countryName
+ *        in: path
+ *        required: true
+ *        schema:
+ *          type: string
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ *      '404':
+ *        description: Country not found
+ *      '500':
+ *        description: Internal Server Error
+ */
+router.get(
+  '/api/countries/:country/cities',
+  async (req, res) => {
+    const countryName = req.params.country
 
     try {
-        const country = await TravelAppCountriesModel.findOne({ name: countryName }, 'cities.name cities.image cities.description -_id');
-        if (!country) {
-            return res.status(404).json({ error: 'Country not found' });
-        }
-        res.json(country);
+      const country = await TravelAppCountriesModel.findOne(
+        { name: countryName },
+        'cities.name cities.image cities.description -_id'
+      )
+      if (!country) {
+        return res
+          .status(404)
+          .json({ error: 'Country not found' })
+      }
+      res.json(country)
     } catch (error) {
-        console.error('Error retrieving country data:', error);
-        res.status(500).send('Internal Server Error');
+      console.error('Error retrieving country data:', error)
+      res.status(500).send('Internal Server Error')
     }
-});
+  }
+)
 
-module.exports = router;
+module.exports = router
--- Endpoints/HotelsGET.js
+++ Endpoints/HotelsGET.js
@@ -1,29 +1,61 @@
-const express = require('express');
-const router = express.Router();
-const TravelAppCountriesModel = require('../Schemas/CountriesSchema');
-
-
-router.get('/:countryName/:cityName/hotels', async (req, res) => {
+const express = require('express')
+const router = express.Router()
+const TravelAppCountriesModel = require('../Schemas/CountriesSchema')
+/**
+ * @swagger
+ * /api/countries/{countryName}/cities/{cityName}/hotels:
+ *  get:
+ *    description: Use to request all hotels of a city in a country
+ *    parameters:
+ *      - name: countryName
+ *        in: path
+ *        required: true
+ *        schema:
+ *          type: string
+ *      - name: cityName
+ *        in: path
+ *        required: true
+ *        schema:
+ *          type: string
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ *      '404':
+ *        description: Country or City not found
+ *      '500':
+ *        description: Internal Server Error
+ */
+router.get(
+  '/api/countries/:countryName/cities/:cityName/hotels',
+  async (req, res) => {
     try {
-        const countryName = req.params.countryName;
-        const cityName = req.params.cityName;
-        const country = await TravelAppCountriesModel.findOne({ name: countryName });
+      const countryName = req.params.countryName
+      const cityName = req.params.cityName
+      const country = await TravelAppCountriesModel.findOne(
+        { name: countryName }
+      )
 
-        if (!country) {
-            return res.status(404).send('Country not found');
-        }
+      if (!country) {
+        return res.status(404).send('Country not found')
+      }
 
-        const city = country.cities.find(city => city.name === cityName);
+      const city = country.cities.find(
+        (city) => city.name === cityName
+      )
 
-        if (!city) {
-            return res.status(404).send('City not found');
-        }
+      if (!city) {
+        return res.status(404).send('City not found')
+      }
 
-        res.send(city.hotels);
+      res.send(city.hotels)
     } catch (error) {
-        console.error('Error retrieving data from collection:', error);
-        res.status(500).send('Internal Server Error');
+      console.error(
+        'Error retrieving data from collection:',
+        error
+      )
+      res.status(500).send('Internal Server Error')
     }
-});
+  }
+)
 
-module.exports = router;
+module.exports = router
