c8eb02b296cd9283a2418e508568daf256de54cc
KajetanMajda <91740670+KajetanMajda@users.noreply.github.com>
2023-12-18 15:40:08 +0100

    Creating 2 endpoint and updating first

---
+++ Endpoints/CountryGet.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const TravelAppCountriesModel = require('../Schemas/CountriesSchema');
+
+router.get('/:country', async (req, res) => {
+    const countryName = req.params.country;
+    try {
+        const country = await TravelAppCountriesModel.findOne({ name: countryName }, 'name image currency population language cities.name -_id');
+        if (!country) {
+            return res.status(404).json({ error: 'Country not found' });
+        }
+        res.json(country);
+    } catch (error) {
+        console.error('Error retrieving country data:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const databaseConnection = require('./DataBaseConnection');
 const countriesGET = require('./Endpoints/CountriesGET');
+const countriesCitiesGET = require('./Endpoints/CitiesGET');
+const countryGET = require('./Endpoints/CountryGet');
 
 const app = express();
 const port = 3000;
@@ -8,6 +10,8 @@ const port = 3000;
 databaseConnection();
 
 app.use(countriesGET);
+app.use(countriesCitiesGET);
+app.use(countryGET);
 
 app.listen(port, () => {
     console.log(`Application works on port: ${port}`);
