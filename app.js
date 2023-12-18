367847351be05b0b8f1496f4107a25b9840823b1
wojciechosmulski <34412964+wojciechosmulski@users.noreply.github.com>
2023-12-18 18:22:08 +0100

    feat: add swagger-ui

--- app.js
+++ app.js
@@ -1,26 +1,49 @@
-const express = require('express');
-const databaseConnection = require('./DataBaseConnection');
-const countriesGET = require('./Endpoints/CountriesGET');
+const express = require('express')
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+const databaseConnection = require('./DataBaseConnection')
+const countriesGET = require('./Endpoints/CountriesGET')
+const attractionsGET = require('./Endpoints/AttractionGET')
+const restaurantsGET = require('./Endpoints/RestaurantsGET')
+const hotelsGET = require('./Endpoints/HotelsGET')
+const countriesCitiesGET = require('./Endpoints/CitiesGET')
+const countryGET = require('./Endpoints/CountryGet')
 
-const attractionsGET = require('./Endpoints/AttractionGET');
-const restaurantsGET = require('./Endpoints/RestaurantsGET');
-const hotelsGET = require('./Endpoints/HotelsGET');
-const countriesCitiesGET = require('./Endpoints/CitiesGET');
-const countryGET = require('./Endpoints/CountryGet');
+const app = express()
+const port = 3000
 
+// Extended: https://swagger.io/specification/#infoObject
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'Travel App API',
+      description: 'Travel App API Information',
+      contact: {
+        name: 'Developer Name',
+      },
+      servers: ['http://localhost:3000'],
+    },
+  },
+  // ['.routes/*.js']
+  apis: ['./Endpoints/*.js'],
+}
 
-const app = express();
-const port = 3000;
+const swaggerDocs = swaggerJsDoc(swaggerOptions)
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocs)
+)
 
-databaseConnection();
+databaseConnection()
 
-app.use(countriesGET);
-app.use(attractionsGET);
-app.use(restaurantsGET);
-app.use(hotelsGET);
-app.use(countriesCitiesGET);
-app.use(countryGET);
+app.use(countriesGET)
+app.use(attractionsGET)
+app.use(restaurantsGET)
+app.use(hotelsGET)
+app.use(countriesCitiesGET)
+app.use(countryGET)
 
 app.listen(port, () => {
-    console.log(`Application works on port: ${port}`);
-});
+  console.log(`Application works on port: ${port}`)
+})
